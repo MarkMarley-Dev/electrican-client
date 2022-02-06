945c5cd521fab9db97c2602c9b0ae1b5996017a5
MarkMarley-Dev <markmarley19911@gmail.com>
2022-02-06 19:42:29 +0000

    Background Image

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,21 @@
+import "./header.module.css";
+
+const Header = () => {
+  return (
+    <header>
+      <ul>
+        <li>
+          <p> About Us</p>
+        </li>
+        <li>
+          <p> Previous Works </p>
+        </li>
+        <li>
+          <p> Get In Touch</p>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
